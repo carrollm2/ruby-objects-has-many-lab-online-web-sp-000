642b472a7f11f740f5cc2a05275c08d3bf8133da
Michael Carroll <carrollm2@gmail.com>
2019-11-01 01:19:47 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,12 +1,15 @@
 class Author
   attr_accessor :name
 
+
   @@all = []
+
 
   def initialize(name)
     @name = name
     @posts = []
   end
+
 
   def add_post(post)
     @posts << post
@@ -14,9 +17,11 @@ class Author
     @@all << self
   end
 
+
   def posts
     Post.all.select{ |post| post.author == self}
   end
+
 
   def add_post_by_title(post_title)
     post = Post.new(post_title)
