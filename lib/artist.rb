4710c7f69db08a68b9bc030d19fafe78fc9f423b
Michael Carroll <carrollm2@gmail.com>
2019-11-01 00:52:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 class Artist
   attr_accessor :name
 
-  @@all_songs = []
+  @@all = []
 
   def initialize(name)
     @name = name
@@ -11,7 +11,7 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    @@all_songs << song
+    @@all << song
   end
 
   def songs
@@ -25,7 +25,7 @@ class Artist
 
 
   def self.song_count
-    @@all_songs.length
+    @@all.length
   end
 
 end
