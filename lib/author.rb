0873a50026e93453fe2efdad0c9db016712e218a
Michael Carroll <carrollm2@gmail.com>
2019-11-01 00:52:52 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,7 @@
 class Author
   attr_accessor :name
 
-  @@all_posts = []
+  @@all = []
 
   def initialize(name)
     @name = name
@@ -11,7 +11,7 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
-    @@all_posts << self
+    @@all << self
   end
 
   def posts
